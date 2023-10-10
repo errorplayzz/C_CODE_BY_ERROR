b0deeba3e3ab5157137cf1d1b812a3c320c1e7f9
ERRORPlayzz <97539044+errorplayzz@users.noreply.github.com>
2023-10-10 20:10:30 +0530

    caculator with working operators

---
+++ calculatorwitho.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+    char operator;
+
+    // Read the numbers and operator from the user
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    // Perform calculations based on the operator
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2f\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2f\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2f\n", result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result: %.2f\n", result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            /*this my calculator for fun*/
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+            break;
+    }
+
+    return 0;
+}
